ac563dad276afdbac2c8a61812fd0f53f1a2ec07
kiyimba1 <edwineddie54@gmail.com>
2020-10-10 01:53:38 +0300

    --- Auto-Git Commit ---

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICurrentWeather } from '../interfaces';
+
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  styleUrls: ['./current-weather.component.css'],
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather;
 
-  constructor() { }
+  constructor() {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: new Date(),
 
-  ngOnInit(): void {
+      image: 'assets/img/sunny.svg',
+      temperature: 72,
+      description: 'sunny',
+    } as ICurrentWeather;
   }
 
+  ngOnInit(): void {}
 }
