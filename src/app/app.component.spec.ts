814eb9e0095b704a53bfe0de8728e91e3e6444e6
kiyimba1 <edwineddie54@gmail.com>
2020-10-10 01:23:16 +0300

    --- Auto-Git Commit ---

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -19,13 +17,14 @@ describe('AppComponent', () => {
   it(`should have as title 'local-weather-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('local-weather-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('local-weather-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'local-weather-app app is running!'
+    );
   });
 });
