d3f1100bf08147d8c57b5b647cb50d2bc4574743
kiyimba1 <edwineddie54@gmail.com>
2020-10-10 02:23:45 +0300

    --- Auto-Git Commit ---

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '1e921ef7b18bf942340a93cf63d5af5b',
+  baseUrl: 'http://',
 };
 
 /*
