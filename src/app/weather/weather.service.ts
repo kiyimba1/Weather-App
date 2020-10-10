d860387cd0632caa4e06e0960896199182f9967c
kiyimba1 <edwineddie54@gmail.com>
2020-10-10 03:14:08 +0300

    Display weather data to the user.

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { ICurrentWeather } from '../interfaces';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 interface ICurrentWeatherData {
   weather: [
@@ -18,7 +19,7 @@ interface ICurrentWeatherData {
   sys: {
     country: string;
   };
-  dt: number;
+  dt: Date;
   name: string;
 }
 
@@ -38,9 +39,28 @@ export class WeatherService {
       .set('q', `${city}, ${country}`)
       .set('appid', environment.appId);
 
-    return this.httpClient.get<ICurrentWeatherData>(
-      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather`,
-      { params: uriParams }
-    );
+    return this.httpClient
+      .get<ICurrentWeatherData>(
+        `${environment.baseUrl}api.openweathermap.org/data/2.5/weather`,
+        { params: uriParams }
+      )
+      .pipe(map((data) => this.transformToICurrentWeather(data)));
+  }
+
+  private transformToICurrentWeather(
+    data: ICurrentWeatherData
+  ): ICurrentWeather {
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: data.dt,
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+      temperature: this.convertKelvinToFahrenheit(data.main.temp),
+      description: data.weather[0].description,
+    };
+  }
+
+  private convertKelvinToFahrenheit(kelvin: number): number {
+    return (kelvin * 9) / 5 - 459.67;
   }
 }
